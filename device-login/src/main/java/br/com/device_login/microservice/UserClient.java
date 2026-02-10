afe76f2592b4854a22886f83b6f2a404e3bc64f5
Pablo <pablorshds@gmail.com>
2026-02-10 15:29:48 -0300

    Adicionando alterações no microserviço de login

--- device-login/src/main/java/br/com/device_login/microservice/UserClient.java
+++ device-login/src/main/java/br/com/device_login/microservice/UserClient.java
@@ -1,12 +1,11 @@
 package br.com.device_login.microservice;
 
 import br.com.device_login.dtos.loginDto.ResponseUserForLogin;
-import br.com.device_login.infra.user_fallback.DeviceUserFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "DEVICE-USER", fallbackFactory = DeviceUserFallBack.class)
+@FeignClient(name = "DEVICE-USER")
 public interface UserClient {
 
      /*
--- device-login/src/main/java/br/com/device_login/service/LoginService.java
+++ device-login/src/main/java/br/com/device_login/service/LoginService.java
@@ -5,8 +5,10 @@ import br.com.device_login.dtos.loginDto.ResponseUserForLogin;
 import br.com.device_login.dtos.tokenDto.RequestTokensDto;
 import br.com.device_login.dtos.tokenDto.ResponseTokens;
 import br.com.device_login.infra.exceptions.InvalidCredentialsException;
+import br.com.device_login.infra.exceptions.ServiceUnavailableException;
 import br.com.device_login.metrics.login.LoginMetrics;
 import br.com.device_login.microservice.UserClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,27 +52,27 @@ public class LoginService {
 
     public ResponseTokens login(RequestLoginDto request) {
 
-        log.info("Iniciando o timer para o serviço de LOGIN");
+        // Inicio o Timer
         var timeSample = this.loginMetrics.startTimer();
 
-        log.info("Iniciando a verficação do usuário");
+        // Faço uma verificação para ver se o usuário existe, e também verifico se o e-mail e a senha estão corretos
         var user = this.verifyUser(request.email(), request.password(), timeSample);
 
-        log.info("Iniciando a geração de tokens");
+        // Retorno os tokens caso o usuário exista
         var tokens = this.generateTokens(user.userId(), user.role());
 
         this.loginMetrics.loginSuccess();
         this.loginMetrics.stopSuccessLoginTimer(timeSample);
-        log.info("Retornando os tokens");
         return tokens;
     }
 
     public ResponseUserForLogin verifyUser(String email, String password, Timer.Sample timeSample) {
-        log.debug("Verifying credentials for email: {}", email);
-        var user = this.userClient.getResponseUserWithEmailOrUserId(email, null);
+        log.debug("Verificando se o e-mail está cadastrado no banco de dados: {}", email);
+        var user = this.getUser(email, null);
 
         if (user == null) {
 
+            log.info("Usuário não encontrado no banco de dados!");
             this.loginMetrics.userNotFound();
             this.loginMetrics.stopFailedLoginTimer(timeSample);
             throw new InvalidCredentialsException("Email or Password is incorrect");
@@ -78,17 +80,21 @@ public class LoginService {
 
         if (!this.passwordEncoder.matches(password, user.password())) {
 
+            log.info("Usuário encontrado, mas a senha está incorreta!");
             this.loginMetrics.invalidCredentials();
             this.loginMetrics.stopFailedLoginTimer(timeSample);
             throw new InvalidCredentialsException("Email or Password is incorrect");
         }
 
-        log.debug("Credentials verified for user: {}", user.userId());
+        log.debug("Usuário verificado com sucesso, retorno suas informações para assim gerar seu token: {}", user.userId());
+
         return user;
     }
 
+    // Método de geração de tokens e refreshTokens
     public ResponseTokens generateTokens(String userId, String role) {
-        log.debug("Generating tokens for user: {}", userId);
+
+        log.debug("Gerando os tokens para o usuário: {}", userId);
         var expireToken = LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
         var now = Instant.now();
 
@@ -113,8 +119,24 @@ public class LoginService {
         var accessToken = this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
         var accessRefreshToken = this.jwtEncoder.encode(JwtEncoderParameters.from(claimsRefresh)).getTokenValue();
 
-        log.debug("Tokens generated for user: {}", userId);
+        log.debug("Tokens gerados com sucesso!");
         return new ResponseTokens(accessToken, accessRefreshToken);
+    }
+
+    // Circuit breaker para o microservice de usuário
+    @CircuitBreaker(name = "circuitbreaker_feign", fallbackMethod = "getUserFallback")
+    public ResponseUserForLogin getUser(String email, String userId ) {
+
+        if (email != null && !email.isBlank()) {
+            return this.userClient.getResponseUserWithEmailOrUserId(email, null);
+        }
+
+        return this.userClient.getResponseUserWithEmailOrUserId(null, userId);
+    }
+
+    public ResponseUserForLogin getUserFallback(String email, String userId, Exception ex) {
+        log.error("Service Unavailable, try again later.", ex);
+        throw new ServiceUnavailableException("Service Unavailable, try again later.");
     }
     // ================================================================================================================
 
@@ -122,33 +144,39 @@ public class LoginService {
 
     // ======================================== REFRESH TOKENS ========================================================
 
-    public ResponseTokens refreshTokens(RequestTokensDto request){
+    public ResponseTokens refreshTokens(RequestTokensDto request) {
 
-        log.info("Iniciando o timer para o serviço de REFRESH-TOKENS");
         var timeSample = this.loginMetrics.startTimer();
 
-        var accessToken = this.jwtDecoder.decode(request.accessToken());
-        var refreshToken = this.jwtDecoder.decode(request.refreshToken());
+        var accessToken = jwtDecoder.decode(request.accessToken());
+        var refreshToken = jwtDecoder.decode(request.refreshToken());
 
-        log.debug("Refresh token expiry: {}, Current time: {}", refreshToken.getExpiresAt(), Instant.now());
-        if (refreshToken.getExpiresAt() == null || Instant.now().isBefore(refreshToken.getExpiresAt())) {
+        // 1. Refresh token deve expirar
+        if (refreshToken.getExpiresAt() == null ||
+                Instant.now().isAfter(refreshToken.getExpiresAt())) {
 
-            var user = this.userClient.getResponseUserWithEmailOrUserId(null,accessToken.getSubject());
-            var tokens = this.generateTokens(user.userId(), user.role());
-
-            // Métrica de contagem de sucesso na renavação de tokens.
-            this.loginMetrics.refreshTokenSuccess();
-            // Métrica de tempo para ver quanto que demora para a renovar os tokens.
-            this.loginMetrics.stopSuccessRefreshTokensTimer(timeSample);
-            // Retorno os novos tokens
-            return tokens;
+            log.debug("Refresh token expirado ou inválido!");
+            this.loginMetrics.failedRefreshTokens();
+            this.loginMetrics.stopFailedRefreshTokensTimer(timeSample);
+            throw new InvalidCredentialsException("Invalid or expired refresh token");
         }
 
-        // Métrica de contagem de falha na renavação de tokens.
-        this.loginMetrics.failedRefreshTokens();
-        // Métrica de tempo para ver quanto que demora para a falha ao não conseguir renovar os tokens.
-        this.loginMetrics.stopFailedRefreshTokensTimer(timeSample);
-        log.warn("Invalid or expired refresh token");
-        throw new InvalidCredentialsException("Invalid or expired refresh token");
+        // 2. Subjects devem bater
+        if (!refreshToken.getSubject().equals(accessToken.getSubject())) {
+
+            log.debug("Subjects não batem!");
+            this.loginMetrics.failedRefreshTokens();
+            this.loginMetrics.stopFailedRefreshTokensTimer(timeSample);
+            throw new InvalidCredentialsException("Invalid refresh token");
+        }
+
+        // 3. Busca usuário confiável
+        var user = this.getUser(null, refreshToken.getSubject());
+        var tokens = this.generateTokens(user.userId(), user.role());
+
+        this.loginMetrics.refreshTokenSuccess();
+        this.loginMetrics.stopSuccessRefreshTokensTimer(timeSample);
+
+        return tokens;
     }
 }
