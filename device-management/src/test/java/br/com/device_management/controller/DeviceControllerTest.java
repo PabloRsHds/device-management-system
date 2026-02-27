aa655637c7612bd4655504269a81d56c93a815fe
Pablo <pablorshds@gmail.com>
2026-02-26 22:27:02 -0300

    + atualizações

--- device-management/src/test/java/br/com/device_management/controller/DeviceControllerTest.java
+++ device-management/src/test/java/br/com/device_management/controller/DeviceControllerTest.java
@@ -233,14 +233,7 @@ class DeviceControllerTest {
                                 "location": "location"
                             }
                         """))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.timesTamp").exists())
-                .andExpect(jsonPath("$.status").exists())
-                .andExpect(jsonPath("$.error").exists())
-                .andExpect(jsonPath("$.source").exists())
-                .andExpect(jsonPath("$.service").exists())
-                .andExpect(jsonPath("$.message").exists())
-                .andExpect(jsonPath("$.path").exists());;
+                .andExpect(status().isBadRequest());
     }
 
     @Test
