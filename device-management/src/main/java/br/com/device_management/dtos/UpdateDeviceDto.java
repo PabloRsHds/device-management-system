9a70d681396469f2524bd551881e6a02f8018c0c
Pablo <pablorshds@gmail.com>
2026-02-26 22:46:24 -0300

    colocando validações no update

--- device-management/src/main/java/br/com/device_management/dtos/UpdateDeviceDto.java
+++ device-management/src/main/java/br/com/device_management/dtos/UpdateDeviceDto.java
@@ -1,10 +1,28 @@
 package br.com.device_management.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UpdateDeviceDto(
+
+        @NotBlank(message = "The name field cannot be blank")
+        @Size(min = 2, max = 30, message = "The name field must have between 2 and 50 characters")
         String newName,
+
+        @NotBlank(message = "The device model field cannot be blank")
+        @Size(min = 2, max = 30, message = "The device model field must have between 1 and 50 characters")
         String newDeviceModel,
+
+        @NotBlank(message = "The manufacturer field cannot be blank")
+        @Size(min = 2, max = 30, message = "The manufacturer field must have between 1 and 50 characters")
         String newManufacturer,
+
+        @NotBlank(message = "The location field cannot be blank")
+        @Size(max = 100, message = "The location field must have a maximum of 100 characters")
         String newLocation,
+
+        @NotBlank(message = "The description field cannot be blank")
+        @Size(max = 200, message = "The description field must have a maximum of 200 characters")
         String newDescription
 ) {
 }
