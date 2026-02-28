4ec6c0aa653641f5f8b7f7a249b0f048056f0eb7
Pablo <pablorshds@gmail.com>
2026-02-28 01:09:27 -0300

    Novos testes sobre validação

--- device-login/src/test/java/br/com/device_login/controller/LoginControllerTest.java
+++ device-login/src/test/java/br/com/device_login/controller/LoginControllerTest.java
@@ -70,6 +70,20 @@ class LoginControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    void shouldReturn400WhenFieldEmailTheSizeIsIncorrectMin() throws Exception{
+
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                {
+                  "email": "A",
+                  "password": "99218841Pp@"
+                }
+            """))
+                .andExpect(status().isBadRequest());
+    }
+
 
 
     @Test
