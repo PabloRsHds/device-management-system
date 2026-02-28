6bfa41ac281ff29e0a526a854d12ca4dfe32cc2d
Pablo <pablorshds@gmail.com>
2026-02-28 15:35:45 -0300

    Novos testes sobre validação

--- device-login/src/test/java/br/com/device_login/infra/global_exceptions/GlobalExceptionHandlerTest.java
+++ device-login/src/test/java/br/com/device_login/infra/global_exceptions/GlobalExceptionHandlerTest.java
@@ -4,6 +4,7 @@ import br.com.device_login.controller.LoginController;
 import br.com.device_login.dtos.loginDto.RequestLoginDto;
 import br.com.device_login.dtos.tokenDto.ResponseTokens;
 import br.com.device_login.infra.exceptions.InvalidCredentialsException;
+import br.com.device_login.infra.exceptions.ServiceUnavailableException;
 import br.com.device_login.metrics.exception.MetricsForExceptions;
 import br.com.device_login.service.LoginService;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,7 @@ class GlobalExceptionHandlerTest {
     private MetricsForExceptions metricsForExceptions;
 
     @Test
-    void shouldReturn401InvalidCredentialsExceptionLogin() throws Exception {
+    void shouldReturn401InvalidCredentialsException() throws Exception {
 
         when(this.loginController.login(any(RequestLoginDto.class)))
                 .thenThrow(new InvalidCredentialsException("Email or Password is incorrect"));
@@ -57,6 +58,31 @@ class GlobalExceptionHandlerTest {
                 .andExpect(jsonPath("$.target").value("USER-DEVICE"))
                 .andExpect(jsonPath("$.service").value("device-login"))
                 .andExpect(jsonPath("$.message").value("Email or Password is incorrect"))
+                .andExpect(jsonPath("$.path").value("/api/login"));
+    }
+
+    @Test
+    void shouldReturn503ServiceUnavailableException() throws Exception {
+
+        when(this.loginController.login(any(RequestLoginDto.class)))
+                .thenThrow(new ServiceUnavailableException("Service unavailable, try later again"));
+
+        this.mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                        {
+                            "email":"teste@gmail.com",
+                            "password":"99218841Pp@"
+                        }
+                        """))
+                .andExpect(status().isServiceUnavailable())
+                .andExpect(jsonPath("$.timesTamp").exists())
+                .andExpect(jsonPath("$.status").value(503))
+                .andExpect(jsonPath("$.error").value("Service unavailable"))
+                .andExpect(jsonPath("$.source").value("DEVICE-LOGIN"))
+                .andExpect(jsonPath("$.target").value("USER-DEVICE"))
+                .andExpect(jsonPath("$.service").value("device-login"))
+                .andExpect(jsonPath("$.message").value("Service unavailable, try later again"))
                 .andExpect(jsonPath("$.path").value("/api/login"));
     }
 }
