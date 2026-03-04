b3d0f65585ab3d8d74c0074852fc15e499283b3d
Pablo <pablorshds@gmail.com>
2026-03-03 22:52:56 -0300

    Alterando testes

--- device-management/src/test/java/br/com/device_management/controller/DeviceControllerTest.java
+++ device-management/src/test/java/br/com/device_management/controller/DeviceControllerTest.java
@@ -752,7 +752,10 @@ class DeviceControllerTest {
                             "newDescription" : "description"
                         }
                         """))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.error").value("Validation error"))
+                .andExpect(jsonPath("$.path").value("/api/update-device/deviceModel"));
     }
 
     @Test
