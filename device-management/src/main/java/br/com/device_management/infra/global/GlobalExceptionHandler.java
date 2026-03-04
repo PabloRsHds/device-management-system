7d99ed07fcf0119ce5fbc98d29bf5265cbae6c2a
Pablo <pablorshds@gmail.com>
2026-03-03 22:39:42 -0300

    Alterando testes

--- device-management/src/main/java/br/com/device_management/infra/global/GlobalExceptionHandler.java
+++ device-management/src/main/java/br/com/device_management/infra/global/GlobalExceptionHandler.java
@@ -6,14 +6,17 @@ import br.com.device_management.infra.exceptions.DeviceIsEmpty;
 import br.com.device_management.infra.exceptions.DeviceIsPresent;
 import br.com.device_management.infra.exceptions.ServiceUnavailable;
 import br.com.device_management.metrics.excepiton.MetricsForExceptions;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -45,6 +48,46 @@ public class GlobalExceptionHandler {
                 ex.getMessage(),
                 request.getRequestURI()
         ));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseExceptionDto> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpServletRequest request) {
+
+        var message = "Invalid value for one of the fields.";
+
+        // Verifica se o erro foi causado por Enum inválido
+        if (ex.getCause() instanceof InvalidFormatException invalidFormatException) {
+
+            if (invalidFormatException.getTargetType().isEnum()) {
+
+                String fieldName = invalidFormatException.getPath().get(0).getFieldName();
+
+                message = "Invalid value for field '" + fieldName +
+                        "'. Allowed values: " +
+                        Arrays.toString(invalidFormatException.getTargetType().getEnumConstants());
+            }
+        }
+
+        this.metrics.recordErrors(new RequestExceptionDto(
+                HttpStatus.BAD_REQUEST.toString(),
+                "Validation error",
+                message,
+                request.getRequestURI())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ResponseExceptionDto(
+                        Instant.now().toString(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Validation error",
+                        "DEVICE-MANAGEMENT",
+                        this.serviceName,
+                        message,
+                        request.getRequestURI()
+                )
+        );
     }
 
     @ExceptionHandler(ServiceUnavailable.class)
