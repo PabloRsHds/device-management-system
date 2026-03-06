63f4a8f521363436d2d670885038294e43a32f72
Pablo <pablorshds@gmail.com>
2026-03-05 21:22:33 -0300

    + testes

--- sensor-test/src/test/java/br/com/sensor_test/integration/SensorIntegration.java
+++ sensor-test/src/test/java/br/com/sensor_test/integration/SensorIntegration.java
@@ -1,10 +1,15 @@
 package br.com.sensor_test.integration;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -15,4 +20,18 @@ public class SensorIntegration {
     private MockMvc mockMvc;
 
 
+    @Test
+    void shouldReturnSuccessWhenUpdateSensor() throws Exception{
+
+        this.mockMvc.perform(patch("/update-sensor/{deviceModel}", "deviceModel")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "name" : "newName",
+                            "deviceModel" : "",
+                            "manufacturer" : ""
+                        }
+                        """))
+                .andExpect(status().isOk());
+    }
 }
