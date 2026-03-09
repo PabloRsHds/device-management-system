1256ab824683a566406228a92c70c7d0904c66bd
Pablo <pablorshds@gmail.com>
2026-03-09 18:13:03 -0300

    Implementando os testes unitários do serviço de registro

--- analysis/src/test/java/br/com/analysis/controller/AnalysisControllerTest.java
+++ analysis/src/test/java/br/com/analysis/controller/AnalysisControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -31,6 +32,23 @@ class AnalysisControllerTest {
     @MockitoBean
     private AnalysisService analysisService;
 
+    private ResultActions expectDefaultErrorStructure(ResultActions result) throws Exception {
+        return result
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.status").exists())
+                .andExpect(jsonPath("$.error").exists())
+                .andExpect(jsonPath("$.source").exists())
+                .andExpect(jsonPath("$.target").exists())
+                .andExpect(jsonPath("$.service").exists())
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.path").exists())
+
+                .andExpect(jsonPath("$.source").value("ANALYSIS"))
+                .andExpect(jsonPath("$.target").value("DATABASE"))
+                .andExpect(jsonPath("$.service").value("analysis"));
+    }
+
+    // ===================================== FindDeviceForAnalysis ===================================================
     @Test
     void shouldReturnResponseDeviceAnalysisDtoWhenFindDeviceForAnalysis() throws Exception {
 
@@ -77,7 +95,11 @@ class AnalysisControllerTest {
 
         this.mockMvc.perform(get("/api/get-device-for-model")
                         .param("deviceModel", "deviceModel"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.error").value("NOT FOUND"))
+                .andExpect(jsonPath("$.message").value("Device not found"))
+                .andExpect(jsonPath("$.path").value("/api/get-device-for-model"));
     }
-
+    // ===============================================================================================================
 }
