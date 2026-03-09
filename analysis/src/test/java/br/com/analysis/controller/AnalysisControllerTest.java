cf5d178319e6315e65152efaa945de25aecb3f36
Pablo <pablorshds@gmail.com>
2026-03-09 18:44:44 -0300

    Implementando os testes unitários do serviço de registro

--- analysis/src/test/java/br/com/analysis/controller/AnalysisControllerTest.java
+++ analysis/src/test/java/br/com/analysis/controller/AnalysisControllerTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -182,5 +181,46 @@ class AnalysisControllerTest {
                 .andExpect(jsonPath("$.error").value("NOT FOUND"))
                 .andExpect(jsonPath("$.message").value("Device not found"))
                 .andExpect(jsonPath("$.path").value("/api/update-analysis/deviceModel"));
+    }
+
+    // ===============================================================================================================
+
+    // ============================================ deleteAnalysis ===================================================
+
+    @Test
+    void shouldReturnResponseDeviceAnalysisDtoWhenDeleteAnalysis() throws Exception{
+
+        var response = new ResponseDeviceAnalysisDto(
+                "name",
+                "deviceModel",
+                0f,
+                100f,
+                "unit",
+                "update",
+                "created",
+                10f,
+                50f,
+                "AA",
+                1,
+                2
+        );
+
+        when(this.analysisService.deleteAnalysis("deviceModel"))
+                .thenReturn(response);
+
+        this.mockMvc.perform(delete("/api/delete-analysis/{deviceModel}", "deviceModel"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.deviceModel").value("deviceModel"))
+                .andExpect(jsonPath("$.minLimit").value(0f))
+                .andExpect(jsonPath("$.maxLimit").value(100f))
+                .andExpect(jsonPath("$.unit").value("unit"))
+                .andExpect(jsonPath("$.updatedAt").value("update"))
+                .andExpect(jsonPath("$.createdAt").value("created"))
+                .andExpect(jsonPath("$.lastReadingMinLimit").value(10f))
+                .andExpect(jsonPath("$.lastReadingMaxLimit").value(50f))
+                .andExpect(jsonPath("$.lastReadingUpdateAt").value("AA"))
+                .andExpect(jsonPath("$.analysisWorked").value(1))
+                .andExpect(jsonPath("$.analysisFailed").value(2));
     }
 }
