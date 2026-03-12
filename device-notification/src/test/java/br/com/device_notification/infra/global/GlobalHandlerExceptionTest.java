d1e054fe8b2dc1fee43176e2d4372f2352c4dce9
Pablo <pablorshds@gmail.com>
2026-03-12 17:48:44 -0300

    Adicionando alterações para testes de integração

--- device-notification/src/test/java/br/com/device_notification/infra/global/GlobalHandlerExceptionTest.java
+++ device-notification/src/test/java/br/com/device_notification/infra/global/GlobalHandlerExceptionTest.java
@@ -1,7 +1,9 @@
 package br.com.device_notification.infra.global;
 
 import br.com.device_notification.controller.NotificationController;
+import br.com.device_notification.infra.exceptions.NotificationNotFound;
 import br.com.device_notification.infra.exceptions.ServiceUnavailable;
+import br.com.device_notification.repository.NotificationRepository;
 import br.com.device_notification.service.NotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(NotificationController.class)
@@ -25,6 +31,9 @@ class GlobalHandlerExceptionTest {
 
     @MockitoBean
     private NotificationService notificationService;
+
+    @MockitoBean
+    private NotificationRepository notificationRepository;
 
     @Test
     void shouldReturnServiceUnavailableWhenCountNotifications() throws Exception{
