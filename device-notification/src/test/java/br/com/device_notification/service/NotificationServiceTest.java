3a71292d5df72817655e95a861171bc5bee071a5
Pablo <pablorshds@gmail.com>
2026-03-11 23:55:21 -0300

    Adicionando alterações para testes de integração

--- device-notification/src/test/java/br/com/device_notification/service/NotificationServiceTest.java
+++ device-notification/src/test/java/br/com/device_notification/service/NotificationServiceTest.java
@@ -1,33 +1,26 @@
 package br.com.device_notification.service;
 
-import br.com.device_notification.dtos.ResponseNotifications;
 import br.com.device_notification.infra.exceptions.NotificationNotFound;
 import br.com.device_notification.metrics.MetricsService;
 import br.com.device_notification.model.Notification;
 import br.com.device_notification.repository.NotificationRepository;
-import jakarta.ws.rs.ServiceUnavailableException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
-
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import static org.mockito.Mockito.*;
-
 
 import java.util.List;
 import java.util.Optional;
 
-import static reactor.core.publisher.Mono.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
