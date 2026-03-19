32369281bec6d611868c1d33397910c25f9b0da1
Pablo <pablorshds@gmail.com>
2026-03-19 13:58:47 -0300

    Alteração no teste de integração

--- device-user/src/main/java/br/com/device_user/microservice/ServiceForLogin.java
+++ device-user/src/main/java/br/com/device_user/microservice/ServiceForLogin.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// Endpoint completo: GET /microservice/verify-if-email-already-cadastred
+
 @RestController
 @RequestMapping("/microservice")
 public class ServiceForLogin {
