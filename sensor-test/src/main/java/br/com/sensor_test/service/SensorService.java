6d7e40d2649081af5988e51bae9de881d3fc6720
Pablo <pablorshds@gmail.com>
2026-04-23 22:17:19 -0300

    fix: alterando de list para page

--- sensor-test/src/main/java/br/com/sensor_test/service/SensorService.java
+++ sensor-test/src/main/java/br/com/sensor_test/service/SensorService.java
@@ -21,7 +21,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
