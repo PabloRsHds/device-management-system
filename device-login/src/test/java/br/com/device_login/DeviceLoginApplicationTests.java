9ae1768d2f519c05770547385639016fa6f9ad69
Pablo <pablorshds@gmail.com>
2026-04-26 00:00:30 -0300

    retirando um teste

--- device-login/src/test/java/br/com/device_login/DeviceLoginApplicationTests.java
+++ device-login/src/test/java/br/com/device_login/DeviceLoginApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.device_login;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeviceLoginApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
