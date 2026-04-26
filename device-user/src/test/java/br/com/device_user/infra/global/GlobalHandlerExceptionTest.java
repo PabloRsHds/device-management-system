31c9af001a957b102de3cffc604bfd64584320b7
Pablo <pablorshds@gmail.com>
2026-04-25 23:55:13 -0300

    fazendo alterações por causa da mudança de requisições para o login

--- device-user/src/test/java/br/com/device_user/infra/global/GlobalHandlerExceptionTest.java
+++ device-user/src/test/java/br/com/device_user/infra/global/GlobalHandlerExceptionTest.java
@@ -3,23 +3,18 @@ package br.com.device_user.infra.global;
 import br.com.device_user.dtos.metricsDto.ExceptionMetricDto;
 import br.com.device_user.infra.exceptions.ServiceUnavailableException;
 import br.com.device_user.metrics.MetricsForExceptions;
-import br.com.device_user.service.user_service.UserService;
 import br.com.device_user.microservice.ServiceForLogin;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
-
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ServiceForLogin.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -35,23 +30,42 @@ class GlobalHandlerExceptionTest {
     private MetricsForExceptions metricsForExceptions;
 
     @Test
-    void shouldReturn503AndFormattedErrorBody() throws Exception {
+    void shouldReturn503AndFormattedErrorBodyEmail() throws Exception {
 
-        when(this.serviceForLogin.getUserForLoginWithEmailOrUserId(any(), any()))
+        when(this.serviceForLogin.getUserByEmail(any()))
                 .thenThrow(new ServiceUnavailableException("Database down"));
 
-        this.mockMvc.perform(get("/microservice/verify-if-email-already-cadastred")
-                        .param("email", "teste@gmail.com")
-                        .param("userId", "123"))
+        this.mockMvc.perform(get("/microservice/verify-by-email")
+                        .param("email", "teste@gmail.com"))
                 .andExpect(status().isServiceUnavailable())
                 .andExpect(jsonPath("$.status").value(503))
                 .andExpect(jsonPath("$.error").value("Service unavailable"))
                 .andExpect(jsonPath("$.service").value("device-user"))
                 .andExpect(jsonPath("$.message").value("Database down"))
                 .andExpect(jsonPath("$.path")
-                        .value("/microservice/verify-if-email-already-cadastred"));
+                        .value("/microservice/verify-by-email"));
 
         verify(metricsForExceptions).recordErrors(any(ExceptionMetricDto.class));
-        verify(this.serviceForLogin).getUserForLoginWithEmailOrUserId(any(), any());
+        verify(this.serviceForLogin).getUserByEmail(any());
+    }
+
+    @Test
+    void shouldReturn503AndFormattedErrorBodyUserId() throws Exception {
+
+        when(this.serviceForLogin.getUserByUserId(any()))
+                .thenThrow(new ServiceUnavailableException("Database down"));
+
+        this.mockMvc.perform(get("/microservice/verify-by-userId")
+                        .param("userId", "userId"))
+                .andExpect(status().isServiceUnavailable())
+                .andExpect(jsonPath("$.status").value(503))
+                .andExpect(jsonPath("$.error").value("Service unavailable"))
+                .andExpect(jsonPath("$.service").value("device-user"))
+                .andExpect(jsonPath("$.message").value("Database down"))
+                .andExpect(jsonPath("$.path")
+                        .value("/microservice/verify-by-userId"));
+
+        verify(metricsForExceptions).recordErrors(any(ExceptionMetricDto.class));
+        verify(this.serviceForLogin).getUserByUserId(any());
     }
 }
